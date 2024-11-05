758104e808d463ca0a812d12c3026409b2e04a16
Dondomie Dungca <ddungca@isbx.com>
2024-11-05 21:14:53 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+const multer = require("multer");
+const path = require("path");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+var storage = multer.diskStorage({
+  destination: path.resolve(process.cwd(), "uploads"),
+  filename: function (req, file, callback) {
+    console.log(file);
+    const ext = path.extname(file.originalname);
+    const name = path.parse(file.originalname).name;
+    callback(null, `${+new Date()}${name + ext}`);
+  },
+});
+var upload = multer({ storage: storage }).single("upfile");
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError || err) {
+      res.send({ error: err.message || "Error on uploading the file" });
+    }
+    res.send({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
